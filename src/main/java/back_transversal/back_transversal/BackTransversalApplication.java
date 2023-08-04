85c9170f33e8d8dc4c4705738be6d18a7309865d
Harena Andriamahatony <harena.andriamahatony@gmail.com>
2023-08-04 16:01:05 +0300

    create project

---
+++ src/main/java/back_transversal/back_transversal/BackTransversalApplication.java
@@ -0,0 +1,13 @@
+package back_transversal.back_transversal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTransversalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackTransversalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/back_transversal/back_transversal/BackTransversalApplicationTests.java
@@ -0,0 +1,13 @@
+package back_transversal.back_transversal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTransversalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
