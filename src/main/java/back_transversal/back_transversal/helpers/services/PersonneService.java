759cb064143fd2e55a2820bcb89f9e49bd35361a
Harena Andriamahatony <harena.andriamahatony@gmail.com>
2023-08-08 15:27:21 +0300

    fixing personne Service error

--- src/main/java/back_transversal/back_transversal/helpers/services/PersonneService.java
+++ src/main/java/back_transversal/back_transversal/helpers/services/PersonneService.java
@@ -3,11 +3,12 @@ package back_transversal.back_transversal.helpers.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import back_transversal.back_transversal.helpers.interfaces.PersonneRepository;
 import back_transversal.back_transversal.models.Personne;
 
-
+@Service
 public class PersonneService {
     private final PersonneRepository personneRepository;
 
