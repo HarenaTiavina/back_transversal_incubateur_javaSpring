be00e37af71542ff0e6e9b557317473fac0457c9
Harena Andriamahatony <harena.andriamahatony@gmail.com>
2023-08-14 20:08:07 +0300

    fix projet

--- src/main/java/back_transversal/back_transversal/models/Projet.java
+++ src/main/java/back_transversal/back_transversal/models/Projet.java
@@ -1,108 +1,83 @@
 package back_transversal.back_transversal.models;
 
-import java.security.Timestamp;
+import java.sql.Timestamp;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
 public class Projet {
+    @Id
     private Long id;
     private String nom;
     private String objectif;
-    private Long idClient;
+    @OneToOne
+    @JoinColumn(name = "id_Client")
+    private  Personne personne;
     private Timestamp datePostule;
     private double capital;
 
     public Projet () {}
 
-    public Projet(Long id, String nom, String objectif, Long idClient, Timestamp datePostule, double capital) {
+    public Projet(Long id, String nom, String objectif, Personne personne, Timestamp datePostule, double capital) {
         this.id = id;
         this.nom = nom;
         this.objectif = objectif;
-        this.idClient = idClient;
+        this.personne = personne;
         this.datePostule = datePostule;
         this.capital = capital;
     }
 
-    /**
-     * @return Long return the id
-     */
     public Long getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
     public void setId(Long id) {
         this.id = id;
     }
 
-    /**
-     * @return String return the nom
-     */
     public String getNom() {
         return nom;
     }
 
-    /**
-     * @param nom the nom to set
-     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
-    /**
-     * @return String return the objectif
-     */
     public String getObjectif() {
         return objectif;
     }
 
-    /**
-     * @param objectif the objectif to set
-     */
     public void setObjectif(String objectif) {
         this.objectif = objectif;
     }
 
-    /**
-     * @return Long return the idClient
-     */
-    public Long getIdClient() {
-        return idClient;
+    public Personne getPersonne() {
+        return personne;
     }
 
-    /**
-     * @param idClient the idClient to set
-     */
-    public void setIdClient(Long idClient) {
-        this.idClient = idClient;
+    public void setPersonne(Personne personne) {
+        this.personne = personne;
     }
 
-    /**
-     * @return Timestamp return the datePostule
-     */
     public Timestamp getDatePostule() {
         return datePostule;
     }
 
-    /**
-     * @param datePostule the datePostule to set
-     */
     public void setDatePostule(Timestamp datePostule) {
         this.datePostule = datePostule;
     }
 
-    /**
-     * @return double return the capital
-     */
     public double getCapital() {
         return capital;
     }
 
-    /**
-     * @param capital the capital to set
-     */
     public void setCapital(double capital) {
         this.capital = capital;
     }
 
+    
+   
 }
