112ebe357e912d293bf753edf49b1731f0f73504
Harena Andriamahatony <harena.andriamahatony@gmail.com>
2023-09-04 15:10:28 +0300

    configure CORS

--- src/main/java/back_transversal/back_transversal/BackTransversalApplication.java
+++ src/main/java/back_transversal/back_transversal/BackTransversalApplication.java
@@ -2,8 +2,11 @@ package back_transversal.back_transversal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+// import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+// @ComponentScan(basePackages = "back_transversal")
 public class BackTransversalApplication {
 
 	public static void main(String[] args) {
